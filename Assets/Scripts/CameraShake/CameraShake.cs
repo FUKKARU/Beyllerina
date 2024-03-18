1d10234da6106198710f7c1dd46d7e9cc2b3c20f
Ajun5GIT <153270000+Ajun5GIT@users.noreply.github.com>
2024-03-18 18:32:21 +0900

    screenshake

---
+++ Assets/Scripts/CameraShake/CameraShake.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    public bool shake = false;
+    [SerializeField] AnimationCurve curve;
+    float duration = 0.3f;
+
+    void Update()
+    {
+        if (shake)
+        {
+            ShakeOn();
+            shake = false;
+        }
+    }
+
+    public void ShakeOn()
+    {
+        StartCoroutine(Shake());
+    }
+
+    IEnumerator Shake()
+    {
+        Vector3 startPosition = transform.position;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            float strength = curve.Evaluate(elapsedTime / duration);
+            transform.position = startPosition + UnityEngine.Random.insideUnitSphere * strength;
+            yield return null;
+        }
+
+        transform.position = startPosition;
+    }
+}
+
