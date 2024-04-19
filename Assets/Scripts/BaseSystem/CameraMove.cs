5e0213d90248b0baf6dad5c4ae39f910b0ef8d73
foriver4725 <tk230001@tks.iput.ac.jp>
2024-04-19 16:57:48 +0900

    camera script check

--- Assets/Scripts/BaseSystem/CameraMove.cs
+++ Assets/Scripts/BaseSystem/CameraMove.cs
@@ -8,17 +8,21 @@ namespace BaseSystem
     {
         [SerializeField] GameObject center;
         [SerializeField] GameObject lookingPosition;
+        float stageRadius = 14.75f;
+        float duration = 60f;
+        float time = 0;
 
         RotateCircle rotInfo;
 
         void Start()
         {
-            //rotInfo = new RotateCircle(center.transform.position, new AxisAngle(Vector3.right, 0), lookingPosition.transform.position);
+            rotInfo = new RotateCircle(gameObject, center.transform.position, stageRadius, new AxisAngle(Vector3.right, 0), lookingPosition.transform.position);
         }
 
         void Update()
         {
-
+            rotInfo.SetPosition(time, 2 * Mathf.PI / duration);
+            time += Time.deltaTime;
         }
     }
 
@@ -29,13 +33,15 @@ namespace BaseSystem
     /// </summary>
     public class RotateCircle
     {
+        public GameObject camera = null;
         public Vector3 center = Vector3.up;
         public float radius = 1;
         public AxisAngle rotation = new AxisAngle(Vector3.right, 0);
         public Vector3 lookingPosition = Vector3.zero;
 
-        public RotateCircle(Vector3 center, float radius, AxisAngle rotation, Vector3 lookingPosition)
+        public RotateCircle(GameObject camera, Vector3 center, float radius, AxisAngle rotation, Vector3 lookingPosition)
         {
+            this.camera = camera;
             this.center = center;
             this.radius = radius;
             this.rotation = rotation;
@@ -51,16 +57,20 @@ namespace BaseSystem
         {
             float angle = time * speed;
 
-            // 1st : Assume that this.rotation is default.
+            // 1st : Assume that this.rotation is default, then calcurate the position on the normalized circle.
             float x = Mathf.Sin(angle);
             float y = -Mathf.Cos(angle);
             float z = 0;
-            Vector3 positionOnNormalizedCenter = new Vector3(x, y, z);
-            Vector3 positionOnCenter = positionOnNormalizedCenter * this.radius + this.center;
+            Vector3 positionOnNormalizedCircle = new Vector3(x, y, z);
 
-            // 2nd : Consider this.rotation.
+            // 2nd : Consider this.rotation, then calcurate the position on the circle.
+            positionOnNormalizedCircle = Quaternion.AngleAxis(rotation.angle, rotation.axis) * positionOnNormalizedCircle;
+            Vector3 positionOnCircle = positionOnNormalizedCircle * this.radius + this.center;
 
             // 3rd : Calcurate the camera's looking position.
+            Vector3 dir = lookingPosition - positionOnCircle;
+            Quaternion rot = Quaternion.LookRotation(dir);
+            camera.transform.rotation = rot * camera.transform.rotation;
         }
     }
 
