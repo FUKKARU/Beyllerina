c3c136baaa43e30da2c5f271cf00194fa0a9ef1d
foriver4725 <tk230001@tks.iput.ac.jp>
2024-04-19 16:36:59 +0900

    tmp

--- Assets/Scripts/BaseSystem/SO/Enemy1StatusSO.cs
+++ Assets/Scripts/BaseSystem/SO/Enemy1StatusSO.cs
@@ -36,5 +36,6 @@ namespace BaseSystem
         [Header("スキル使用時何倍大きくなるか")] public float SkillSizeCoef;
         [Header("スキル使用時どこまで上昇するか")] public float SkillHeight;
         [Header("スキル使用時どのようなスピードで上昇するか")] public float SkillSpeed;
+        [Header("スキルの巨大化を何秒継続するか")] public float a;
     }
 }
