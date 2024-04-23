c02c090eab338b935201afa1ce1c6cf7537a4661
foriver4725 <tk230001@tks.iput.ac.jp>
2024-04-23 18:31:14 +0900

    リセット

--- Assets/Scripts/BaseSystem/GameManager.cs
+++ Assets/Scripts/BaseSystem/GameManager.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
-using static UnityEngine.GraphicsBuffer;
-using Unity.VisualScripting;
 
 namespace BaseSystem
 {
@@ -42,9 +40,6 @@ namespace BaseSystem
         [Header("KOしたUI")] public GameObject KO_UI;
         [Header("KOされたUI")] public GameObject KOed_UI;
         [Header("Now Loading のテキスト")] public TextMeshProUGUI NowLoadingText;
-        [Header("プレイアブルの名前UI")] public GameObject PlayableNameUI;
-        [Header("アンプレイアブルの名前UI")] public GameObject UnPlayableNameUI;
-        [Header("ゲームシーンを中継しているカメラ")] public Camera GameCamera;
 
 
 
@@ -79,10 +74,6 @@ namespace BaseSystem
         {
             // バーを減少させる。
             ChangeBarsFillAmount();
-
-            // 名前UIの表示（プレイアブルのUIが優先）
-            ShowNameUI(U_Pm.gameObject, UnPlayableNameUI);
-            ShowNameUI(P_Pm.gameObject, PlayableNameUI);
         }
 
         void ChangeBarsFillAmount()
@@ -116,29 +107,6 @@ namespace BaseSystem
                     IsChangeUnPlayableBar = false;
                 }
             }
-        }
-
-        void ShowNameUI(GameObject constraint, GameObject obj)
-        {
-#if false
-            // オブジェクトのワールド座標→スクリーン座標変換
-            var targetScreenPos = GameCamera.WorldToScreenPoint(constraint.transform.position);
-
-            // スクリーン座標変換→UIローカル座標変換
-            RectTransformUtility.ScreenPointToLocalPointInRectangle(canvasRt, targetScreenPos, null, out Vector2 uiLocalPos);
-
-            // RectTransformのローカル座標を更新
-            obj.transform.localPosition = uiLocalPos;
-#else
-            // オブジェクトのワールド座標→スクリーン座標変換
-            var targetScreenPos = GameCamera.WorldToScreenPoint(constraint.transform.position + constraint.transform.up*2);
-
-            // スクリーン座標変換→UIローカル座標変換
-            RectTransformUtility.ScreenPointToLocalPointInRectangle(obj.GetComponent<RectTransform>(), targetScreenPos, null, out Vector2 uiLocalPos);
-
-            // RectTransformのローカル座標を更新
-            obj.transform.localPosition = uiLocalPos;
-#endif
         }
 
         #region 勝利/敗北
