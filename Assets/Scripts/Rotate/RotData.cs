9c097d907741d0d84aa1ec7f2ee23770b9a02c50
foriver4725 <tk230001@tks.iput.ac.jp>
2024-04-24 23:40:34 +0900

    回転しないバグ修正

--- Assets/Scripts/Rotate/RotData.cs
+++ Assets/Scripts/Rotate/RotData.cs
@@ -3,7 +3,6 @@ namespace SelectTeam
     public static class RotData 
     {
         // ‰Šú‰ñ“]‘¬“x‚ÉæZ‚³‚ê‚éB
-        public static float RotateNumber { get; set; }
+        public static float RotateNumber { get; set; } = 1;
     }
 }
-
