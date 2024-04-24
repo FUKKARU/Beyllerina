2ecbc5a35a52d96d50a17ed023b747b612fbe065
foriver4725 <tk230001@tks.iput.ac.jp>
2024-04-24 12:39:48 +0900

    Rotateシーン改善

--- Assets/Scripts/Rotate/ControllerRotate.cs
+++ Assets/Scripts/Rotate/ControllerRotate.cs
@@ -7,36 +7,40 @@ namespace SelectTeam
 {
     public class ControllerRotate : MonoBehaviour
     {
+        [SerializeField] GameObject stickSphere;
+        [SerializeField] GameObject stickSphereCenter;
         [SerializeField] TextMeshProUGUI text;
-        [SerializeField] float degree02 = 0;
-
-        void Start()
-        {
-            if (GameManager.Instance.IsUsingMouse)
-            {
-                enabled = false;
-            }
-        }
+        bool isFirstInput = true;
+        float preDeg = 0; // 前フレームの角度
+        float increaseSpeed = 1;
 
         void Update()
         {
-            var h = Input.GetAxis("Horizontal");
-            var v = Input.GetAxis("Vertical");
+            Vector2 rot = IA.InputGetter.Instance.ValueRotate;
+            float deg = Mathf.Atan2(rot.y, rot.x) * Mathf.Rad2Deg;
 
-            float degree = Mathf.Atan2(v, h) * Mathf.Rad2Deg;
-
-            if (degree < 0)
+            if (isFirstInput)
             {
-                degree += 360;
-                degree02 += (degree * 0 + 1) * 1 / 3;
+                isFirstInput = false;
+            }
+            else
+            {
+                float deltaDeg = deg - preDeg;
+                increaseSpeed += Mathf.Abs(deltaDeg) * 0.025f / 360;
             }
 
-            text.text = degree02.ToString();
+            preDeg = deg;
+
+            Vector3 targetPos = rot;
+            targetPos.z = -0.3f;
+            stickSphere.transform.localPosition = targetPos;
+
+            text.text = $"× {increaseSpeed.ToString("f2")}";
         }
 
         private void OnDisable()
         {
-            SceneChange.RotateNumber = degree02 * 2;
+            SceneChange.RotateNumber = increaseSpeed;
         }
     }
 }
--- Assets/Scripts/Rotate/SceneChange.cs
+++ Assets/Scripts/Rotate/SceneChange.cs
@@ -2,6 +2,7 @@ namespace SelectTeam
 {
     public static class SceneChange 
     {
+        // ‰Šú‰ñ“]‘¬“x‚ÉæZ‚³‚ê‚éB
         public static float RotateNumber { get; set; }
     }
 }
